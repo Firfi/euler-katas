85e19460c1745d7b33296910cd0fc37c0ebfdbc0
Igor Loskutov <igor.loskutoff@gmail.com>
2014-05-24 16:49:16 +0700

    DigitFifthPowers

---
+++ src/main/scala/katas/DigitFifthPowers.scala
@@ -0,0 +1,12 @@
+package katas
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: firfi
+ * Date: 5/24/14
+ * Time: 3:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+object DigitFifthPowers {
+
+}
