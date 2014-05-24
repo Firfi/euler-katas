38a02ae6b7a10f3dbf763a04584861e7a5bd4237
Igor Loskutov <igor.loskutoff@gmail.com>
2014-05-24 16:49:26 +0700

    DigitFifthPowers

--- src/main/scala/katas/DigitFifthPowers.scala
+++ src/main/scala/katas/DigitFifthPowers.scala
@@ -8,5 +8,16 @@ package katas
  * To change this template use File | Settings | File Templates.
  */
 object DigitFifthPowers {
-
+  def apply(pow: Int) = {
+    def sumOfPowers(num: Int): Int = num.toString.map(_.toInt - 48).map(Math.pow(_, pow)).sum.toInt
+    val cap = { // function decimalNumber(n) where n is count of digits grows faster than sumOfPowers(decimalNumber(n))
+      Stream.from(1).map(n => n->sumOfPowers(List.fill(n)(9).zipWithIndex.foldLeft(0)({
+        case (acc, (digit, i)) => (acc + digit * Math.pow(10, i)).toInt
+      }))).takeWhile({
+        case (n, s) => s.toString.length > n
+      }).last._2
+    }
+    Stream.from(10).takeWhile(_ <= cap).map(n => n->sumOfPowers(n))
+      .filter({case (num, sop) => num == sop}).map(_._1).sum
+  }
 }
