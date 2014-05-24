bb84446b80b3a2050afe774461d2662dae0bb3ed
Igor Loskutov <igor.loskutoff@gmail.com>
2014-05-24 15:10:04 +0700

    NumberSpiralDiagonals

---
+++ src/main/scala/katas/NumberSpiralDiagonals.scala
@@ -0,0 +1,12 @@
+package katas
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: firfi
+ * Date: 5/24/14
+ * Time: 2:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+object NumberSpiralDiagonals {
+
+}
